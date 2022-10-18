cf7ebea459798759d716320530529fcab2269018
Saippua <ollikos92@gmail.com>
2022-10-18 15:56:13 +0300

    add.js test to test mocha and chai

---
+++ test/add.test.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+import add from '../src/add.js';
+
+
+describe('Add', function() {
+    // it('6 + 4 should be 10', function() {
+    //     expect(add(6, 4)).to.be.equal(10);
+    // });
+    // it('-5 + 4 should be -1', function() {
+    //     expect(add(-5, 4)).to.be.equal(-1);
+    // });
+});
+
