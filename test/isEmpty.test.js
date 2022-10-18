64fa53e4e88a5cfff16443ab5ddd7c5bbb38d5f7
Saippua <ollikos92@gmail.com>
2022-10-18 18:39:42 +0300

    fixed broken test

--- test/isEmpty.test.js
+++ test/isEmpty.test.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import isEmpty from '../src/isEmpty.js'
 
 describe('isEmpty', function() {
-    it('[1] should be empty', function() {
-        expect(isEmpty([1])).to.be.true
+    it('[] should be empty', function() {
+        expect(isEmpty([])).to.be.true
     });
 });
