a7e40a76dfaf4ae7d5501f91893fdfd25c65fb86
Saippua <ollikos92@gmail.com>
2022-10-18 17:55:20 +0300

    uncommented add tests

--- test/add.test.js
+++ test/add.test.js
@@ -3,11 +3,11 @@ import add from '../src/add.js';
 
 
 describe('Add', function() {
-    // it('6 + 4 should be 10', function() {
-    //     expect(add(6, 4)).to.be.equal(10);
-    // });
-    // it('-5 + 4 should be -1', function() {
-    //     expect(add(-5, 4)).to.be.equal(-1);
-    // });
+    it('6 + 4 should be 10', function() {
+        expect(add(6, 4)).to.be.equal(10);
+    });
+    it('-5 + 4 should be -1', function() {
+        expect(add(-5, 4)).to.be.equal(-1);
+    });
 });
 
