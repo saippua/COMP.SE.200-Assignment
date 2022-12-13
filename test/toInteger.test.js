6d3daf09505038cc599b716dd58b0de81d85b017
Saippua <ollikos92@gmail.com>
2022-12-13 19:21:53 +0200

    added tests for toInteger.js fg

---
+++ test/toInteger.test.js
@@ -0,0 +1,46 @@
+import { expect } from 'chai';
+import toInteger from '../src/toInteger.js';
+
+describe('toInteger', function() {
+    describe('Numbers to integer', function() {
+        it('Integer', function() {
+            expect(toInteger(51)).to.be.equal(51);
+            expect(toInteger(-102)).to.be.equal(-102);
+            expect(toInteger(-0)).to.be.equal(0);
+        });
+        it('Float', function() {
+            expect(toInteger(85.3)).to.be.equal(85);
+            expect(toInteger(85.9)).to.be.equal(85);
+            expect(toInteger(-91.2)).to.be.equal(-91);
+        });
+        it('Number', function() {
+            expect(toInteger(Number.MIN_VALUE)).to.be.equal(0);
+            expect(toInteger(new Number(5))).to.be.equal(5);
+            expect(toInteger(new Number(-100))).to.be.equal(-100);
+        });
+    });
+    describe('String to integer', function() {
+        it('Integer string', function() {
+            expect(toInteger('642')).to.be.equal(642);
+            expect(toInteger('-32')).to.be.equal(-32);
+            expect(toInteger('-0')).to.be.equal(0);
+            expect(toInteger('0')).to.be.equal(0);
+        });
+        it('Float string', function() {
+            expect(toInteger('82.2')).to.be.equal(82);
+            expect(toInteger('-52.8')).to.be.equal(-52);
+        });
+        it('Scientific string', function() {
+            expect(toInteger('1.5e2')).to.be.equal(150);
+            expect(toInteger('-1.982e3')).to.be.equal(-1982);
+            expect(toInteger('200e-1')).to.be.equal(20);
+        });
+    });
+    // describe('Unexpected values', function() {
+    //     it('Invalid strings', function() {
+    //         expect(toInteger('test')).to.be.equal(NaN);
+    //     });
+    // });
+});
+
+
