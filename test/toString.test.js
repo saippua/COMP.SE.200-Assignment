fc08dca560c1425a4a4c0a23b90a0c3018c97b62
Saippua <ollikos92@gmail.com>
2022-12-13 17:33:11 +0200

    added tests for toString.js

---
+++ test/toString.test.js
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+import toString from '../src/toString.js';
+
+describe('toString', function() {
+    it('Number should be converted to strings', function() {
+        expect(toString(-19), '-19').to.be.equal('-19')
+        expect(toString(-0), '-0').to.be.equal('-0')
+        expect(toString(0), '0').to.be.equal('0')
+        expect(toString(28), '28').to.be.equal('28')
+    });
+    it('Undefined value should return empty string', function() {
+        expect(toString(undefined)).to.be.equal('');
+    });
+    it('Null value should return empty string', function() {
+        expect(toString(null)).to.be.equal('');
+    });
+    it('Arrays should return elements joined with commas', function() {
+        expect(toString([0, 1, 2]), '[0, 1, 2]').to.be.equal('0,1,2');
+        expect(toString([-0, null, 2]), '[-0, null, 2]').to.be.equal('-0,,2');
+    });
+});
+
