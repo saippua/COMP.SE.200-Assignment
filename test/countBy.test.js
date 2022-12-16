ad594d41dea55d6ce597c7b89b8924fcd64489f1
Saippua <ollikos92@gmail.com>
2022-12-16 13:39:35 +0200

    grouped and added more tests for countBy

--- test/countBy.test.js
+++ test/countBy.test.js
@@ -11,10 +11,20 @@ describe('countBy', function() {
         { 'user': 'Velma',  'active': true,  'points': 2, 'referrer': 'Betty' },
         { 'user': 'Marie',  'active': false, 'points': 1, 'referrer': 'Barney' },
     ];
-    it('Count by property of object', function() {
+    const products = {
+        'tomato': { 'color': 'red' },
+        'pepper': { 'color': 'red' },
+        'flower': { 'color': 'red' },
+        'bean': { 'color': 'green' },
+        'salad': { 'color': 'green'}
+    };
+    it('Count by property of list of objects', function() {
         expect(countBy(users, value => value.active)).to.deep.equal({'true': 3, 'false': 4});
         expect(countBy(users, value => value.points)).to.deep.equal({'1': 4, '2': 2, '3': 1});
         expect(countBy(users, value => value.referrer)).to.deep.equal({'null': 4, 'Barney': 2, 'Betty': 1});
+    });
+    it('Count by property of object', function() {
+        expect(countBy(products, value => value.color)).to.deep.equal({'red': 3, 'green': 2});
     });
 });
 
